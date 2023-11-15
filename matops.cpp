2edee8f7d7821881271427b67b956fbec5f136f1
Daniel <daniel.vasconcelosgomes@gmail.com>
2023-11-15 11:07:56 -0300

    remove

--- matops.cpp
+++
@@ -1,9 +0,0 @@
-#include "matops.h"
-
-using namespace cv;
-
-bool convertOpenCVMathToEingein(const Mat& OpenCVImage)
-{
-	// void cv2eigen(const Mat& src, Eigen::Matrix<_Tp, _rows, _cols, _options, _maxRows, _maxCols>& dst)
-	// void eigen2cv(const Eigen::Matrix<_Tp, _rows, _cols, _options, _maxRows, _maxCols>&src, Mat & dst)
-}
